4ada94bae03f4150ae3d47725e3177da1effc800
DandinPower <t108590045@ntut.org.tw>
2022-05-31 14:45:06 +0800

    set encoder bits quant

--- models/bert/encoder.py
+++ models/bert/encoder.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import sys
 import time 
-kernel =  tf.load_op_library('./bits_quant.so')
 
 class AddParameter(tf.keras.layers.Layer):
     def __init__(self, nums,hiddens):
@@ -9,11 +8,12 @@ class AddParameter(tf.keras.layers.Layer):
         self.w = self.add_variable(name='weight',shape=[nums,hiddens], initializer=tf.zeros_initializer())
 
     def call(self, inputs):
-        return kernel.bits_quant(inputs + self.w)
+        return inputs + self.w
 
 class BERTEncoder(tf.keras.Model):
     def __init__(self, config, parameters):
         super(BERTEncoder, self).__init__()
+        self.kernel = tf.load_op_library('./bits_quant.so')
         self.token_embedding = tf.keras.layers.Embedding(config.vocabSize, config.numHiddens,input_length=config.maxLen,weights=[parameters["encoder.token_embedding.weight"]])
         self.segment_embedding = tf.keras.layers.Embedding(2, config.numHiddens,input_length=config.maxLen,weights=[parameters["encoder.segment_embedding.weight"]])
         self.pos_embedding = AddParameter(config.maxLen,config.numHiddens)
@@ -22,10 +22,9 @@ class BERTEncoder(tf.keras.Model):
 
     def call(self, inputs):
         (tokens,segments) = inputs
-        X = self.token_embedding(tokens)
-        X = X + self.segment_embedding(segments)
-        X = self.pos_embedding(X)
-        print(X.numpy())
+        X = self.kernel.bits_quant(self.token_embedding(tokens))
+        X = self.kernel.bits_quant(X + self.kernel.bits_quant(self.segment_embedding(segments)))
+        X = self.kernel.bits_quant(self.pos_embedding(X))
         return X
 
     def LoadParameters(self):
